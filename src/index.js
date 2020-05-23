2a75ef1ee3be1df3378435327ce945400aa07c5d
koji-koji <k.koyama37@gmail.com>
2020-05-23 09:42:53 +0900

    :dizzy: boardからgameにstateをリフトアップ。

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const Square = (props) => {
   return (
     <button
       className="square"
-      // this.props.onClick()で、BoardのonClickイベントハンドラをコールする。その結果、handleClickがコールされる
       onClick={props.onClick}
     >
       {props.value}
@@ -17,48 +16,19 @@ const Square = (props) => {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
-  }
-
-  handleClick(i) {
-    // イミュータブルにしたいので、constで書いて、新しく設定している。
-    // これにより、巻き戻しの実装が簡単になる。
-    // ミュータブルだとオブジェクトツリーの全体を走査するよ必要がある。
-    // pure componentを構築しやすい。
-    const squares = this.state.squares.slice();
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => {this.handleClick(i)}}
+        value={this.props.squares[i]}
+        onClick={() => {this.props.onClick(i)}}
       />
     )
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    const status = winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -80,14 +50,57 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    // イミュータブルにしたいので、constで書いて、新しく設定している。
+    // これにより、巻き戻しの実装が簡単になる。
+    // ミュータブルだとオブジェクトツリーの全体を走査するよ必要がある。
+    // pure componentを構築しやすい。
+    const squares = this.state.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{squares: squares}]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    const status = winner
+      ? 'Winner: ' + winner
+      : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            // このiがなぜ機能するかを確認する。
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
